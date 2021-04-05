37b7cf8fc36886a13f5864dd5daf06b44a747d3f
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-04-05 10:59:47 -0300

    finished withdraw screen

--- app/src/main/java/tk/williamsouza/ivegan/fragments/HomeFragment/WithdrawFragment.kt
+++ app/src/main/java/tk/williamsouza/ivegan/fragments/HomeFragment/WithdrawFragment.kt
@@ -5,9 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import tk.williamsouza.ivegan.R
+import tk.williamsouza.ivegan.RestaurantsDataSource
+import tk.williamsouza.ivegan.TopSpacingItemDecoration
+import tk.williamsouza.ivegan.adapters.RestaurantsAdapter
 
 class WithdrawFragment : Fragment() {
+    private lateinit var restaurantsAdapter : RestaurantsAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +28,27 @@ class WithdrawFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_withdraw, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+        addDataSet()
+    }
+
+    private fun addDataSet() {
+        val data = RestaurantsDataSource.createDataSet()
+        restaurantsAdapter.submitList(data)
+    }
+
+    private fun initRecyclerView() {
+
+        val restaurantsView = view?.findViewById<RecyclerView>(R.id.restaurantsView)
+        restaurantsView!!.layoutManager = LinearLayoutManager(this.context)
+        val topSpacingDecoration = TopSpacingItemDecoration(30)
+        restaurantsView.addItemDecoration(topSpacingDecoration)
+        restaurantsAdapter = RestaurantsAdapter()
+        restaurantsView?.adapter = restaurantsAdapter
     }
 
 }
